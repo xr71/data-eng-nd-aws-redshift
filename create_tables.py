bf5d1a984bfa31935cf6f8791a93e7c4ce43937e
Xu Ren <musicalxu@hotmail.com>
2019-08-02 17:13:22 -0400

    fix docstrings

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,32 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    This function takes in a cursor object from Redshift via the Psycopg2 library
+    and a connection object. It runs through a series of DROP statements and 
+    drops all of the tables in Redshift for this ETL pipeline should they exist.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    This function takes in a cursor object from Redshift via the Psycopg2 library
+    and a connection object. It runs through a series of CREATE statements and 
+    creates new tables based on the DDL in sql_queries.py 
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Executes the previous ETL statements while orchestrating the config and 
+    connection to Redshift.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- etl.py
+++ etl.py
@@ -4,18 +4,33 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    This function takes in a cursor object from Redshift via the Psycopg2 library
+    and a connection object. It runs through a series of COPY statements and loads
+    data from S3 into staging tables in Redshift. 
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    This function takes in a cursor object from Redshift via the Psycopg2 library
+    and a connection object. It runs through a series of INSERT statements that 
+    takes data from the staging tables and creates the star-schema tables for 
+    the warehouse. 
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Executes the previous ETL statements while orchestrating the config and 
+    connection to Redshift.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
